d759e1d5c23847c98325c53a9266f6ba83a9581e
lauren <lmccauley.email@gmail.com>
2022-01-06 15:01:34 -0500

    Uses module instead of class or inheritance

---
+++ module_example.rb
@@ -0,0 +1,41 @@
+module Vehicle
+  attr_accessor :make, :model
+
+  def initialize
+    @speed = 0
+    @direction = 'north'
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+end
+
+class Car
+  include Vehicle
+
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+end
+
+class Bike
+  include Vehicle
+
+  def ring_bell
+    puts "Ring ring!"
+  end
+end
+car = Car.new
+bike = Bike.new
+p bike.accelerate
+bike.make = "cannondale"
+p bike.make
